335c705033f248a896ab820ff1a6bc104c41e430
VISHAL9340 <164560504+VISHAL9340@users.noreply.github.com>
2025-03-04 20:46:27 +0530

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Create downloads folder if not exists
+    os.makedirs("downloads", exist_ok=True)
+
+    # Import routes
+    from api.routes import download_bp
+    app.register_blueprint(download_bp)
+
+    return app
 
